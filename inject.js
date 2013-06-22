08b6a3c88955882ea541e1bd20e9f8973bc4cbf5
Ben Newman <bn@cs.stanford.edu>
2013-06-22 15:15:23 -0400

    Force type="text/javascript" for enable.js script.

--- inject.js
+++ inject.js
@@ -1,4 +1,6 @@
 window.location = 'javascript:' +
-  'document.documentElement.firstChild.appendChild(' +
-    'document.createElement("script")' +
-  ').setAttribute("src","https://raw.github.com/benjamn/qed-snippets/master/enable.js");';
+  '(function(s){' +
+    's.setAttribute("type","text/javascript");' +
+    's.setAttribute("src","https://raw.github.com/benjamn/qed-snippets/master/enable.js");'+
+    'document.documentElement.firstChild.appendChild(s);' +
+  '}(document.createElement("script")));';
