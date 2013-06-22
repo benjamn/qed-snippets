06fee4fbcfa4137cfa39ffaf40f4fcfe3c128131
Ben Newman <bn@cs.stanford.edu>
2013-06-22 15:20:49 -0400

    Add [code html] snippet and remove second tab stop from [code] snippet.

--- enable.js
+++ enable.js
@@ -1,6 +1,7 @@
 require.enqueue(function(require) {
   var snippets = require("qed/snippets");
   snippets.addTabTrigger("m", "[math]$1[/math]");
-  snippets.addTabTrigger("c", "[code$2]$1[/code]");
-  snippets.addTabTrigger("js", "[code ${2:js}]$1[/code]");
+  snippets.addTabTrigger("c", "[code]$1[/code]");
+  snippets.addTabTrigger("j", "[code ${2:js}]$1[/code]");
+  snippets.addTabTrigger("h", "[code ${2:html}]$1[/code]");
 });
