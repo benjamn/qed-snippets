8d8018412a7e6f2cecfb90a843299685afdd06ef
Ben Newman <bn@cs.stanford.edu>
2013-06-22 15:12:18 -0400

    Add [code] examples demonstrating multiple and default-value tab stops.

--- enable.js
+++ enable.js
@@ -1,3 +1,6 @@
 require.enqueue(function(require) {
-  require("qed/snippets").addTabTrigger("m", "[math]$1[/math]");
+  var snippets = require("qed/snippets");
+  snippets.addTabTrigger("m", "[math]$1[/math]");
+  snippets.addTabTrigger("c", "[code$2]$1[/code]");
+  snippets.addTabTrigger("js", "[code ${2:js}]$1[/code]");
 });
